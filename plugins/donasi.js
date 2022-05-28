4a91f022996f9e47522b4cebe8bf3bb3c6165705
Sabo Ofc <103872974+SaboBOTZMD@users.noreply.github.com>
2022-05-28 12:18:43 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,10 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 ┌─「 Donasi • Pulsa 」
-│ • *Indosat:* [${global.ppulsa}]
-│ • *Axis:* [${global.ppulsa2}]
+│ • *Telkomsel:* [${global.ppulsa}]
 ❏────
 
 ┌─「 Donasi • Non Pulsa 」
-│ • *Ovo:* [${global.povo}]
 │ • *Gopay:* [${global.pgopay}]
 │ • *Dana:* [${global.pdana}]
 ❏────
